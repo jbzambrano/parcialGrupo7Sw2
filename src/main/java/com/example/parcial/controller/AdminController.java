de2a9e8708482ec52ec8602f6132886e44ff913a
JZambrano <jbzambrano@pucp.pe>
2020-06-03 22:16:04 -0500

    Seguimos llorando

--- src/main/java/com/example/parcial/controller/AdminController.java
+++ src/main/java/com/example/parcial/controller/AdminController.java
@@ -10,6 +10,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
+import java.sql.SQLOutput;
 import java.util.Optional;
 
 @Controller
@@ -27,34 +29,48 @@ public class AdminController {
 
     @GetMapping("/new")
     public String nuevoGestor(@ModelAttribute("usuario") Usuario usuario,
-                                   Model model) {
+                              Model model) {
 
         return "admin/form";
     }
 
 
     @PostMapping("/save")
-    public String guardarGestor(@ModelAttribute("usuario") Usuario usuario
-                                  ,Model model, RedirectAttributes attr) {
-        Integer dni= usuario.getDni();
-        Optional<Usuario> opt = usuarioRepository.findById(dni);
+    public String guardarGestor(@ModelAttribute("usuario") @Valid Usuario usuario
+            , BindingResult bindingResult
+            , Model model, RedirectAttributes attr) {
+
         usuario.setIdrol(2);
+        System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+        System.out.println(usuario.getDni());
+        if (usuario.getDni()==null) {
+            model.addAttribute("msgError", "Debe rellenar el parámetro de DNI");
+            return "admin/form";
 
 
-            if (opt.isPresent()) {
-            attr.addFlashAttribute("msg", "Gestor " + "actualizado"+ " exitosamente");
+        } else {
+            if (bindingResult.hasErrors()) {
 
+                return "admin/form";
             } else {
-            attr.addFlashAttribute("msg", "Gestor " + "creado" + " exitosamente");
+                Integer dni = usuario.getDni();
+                Optional<Usuario> opt = usuarioRepository.findById(dni);
+                if (opt.isPresent()) {
+                    attr.addFlashAttribute("msg", "Gestor " + "actualizado" + " exitosamente");
+
+                } else {
+                    attr.addFlashAttribute("msg", "Gestor " + "creado" + " exitosamente");
+                }
+                usuarioRepository.save(usuario);
+                return "redirect:/admin";
             }
-            usuarioRepository.save(usuario);
-            return "redirect:/admin";
+        }
 
     }
 
     @GetMapping("/edit")
     public String editarGestor(@ModelAttribute("usuario") Usuario usuario,
-                                      Model model, @RequestParam("id") int dni) {
+                               Model model, @RequestParam("id") int dni) {
 
         Optional<Usuario> optUsuario = usuarioRepository.findById(dni);
         if (optUsuario.isPresent()) {
@@ -74,7 +90,7 @@ public class AdminController {
         if (optional.isPresent()) {
             usuarioRepository.deleteById(dni);
         }
-        attr.addFlashAttribute("msg","usuario borrado exitosamente");
+        attr.addFlashAttribute("msg", "usuario borrado exitosamente");
         return "redirect:/admin";
     }
 
--- src/main/java/com/example/parcial/entity/Usuario.java
+++ src/main/java/com/example/parcial/entity/Usuario.java
@@ -4,16 +4,27 @@ package com.example.parcial.entity;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.*;
 
 @Entity
 @Table(name="usuario")
 public class Usuario {
 
     @Id
+    @Digits(integer=8,fraction=0,message="El parámetro ha ingresar debe de tener 8 digitos numéricos")
+    @Min(0)
+    @Max(99999999)
     private Integer dni;
+    @Max(value=3)
+    @Min(value=1)
     private Integer idrol;
+    @Size(max=20,message= "Este campo no puede tener más de 20 caracteres")
+    @NotBlank(message="Este campo no puede estar vacio")
     private String nombre;
+    @Size(max=20,message= "Este campo no puede tener más de 20 caracteres")
+    @NotBlank(message="Este campo no puede estar vacio")
     private String apellido;
+    @Email(message="Please provide a valid email address")
     private String correo;
     private String password;
     private Integer activo;
